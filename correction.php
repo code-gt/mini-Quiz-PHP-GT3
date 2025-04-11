7b199b06a6dba9e658e8e33c8cdcb77caab6d112
SebDVS Web <69651784+sebdvsweb@users.noreply.github.com>
2025-04-11 08:39:44 +0200

    Create correction.php

---
+++ correction.php
@@ -0,0 +1,29 @@
+<?php
+
+// Démarrer la session
+
+// Bonnes réponses dans un tableau associatif
+
+// Correction
+
+// Stockage score global
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Correction</title>
+</head>
+<body>
+  <main class="container">
+    <h1>Résultats du quiz</h1>
+    
+    <!-- afficher le score -->
+
+    <!-- ajouter des boutons pour recommencer le quiz ou le réinitialiser -->
+
+  </main>
+</body>
+</html>
