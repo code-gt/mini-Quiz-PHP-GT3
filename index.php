62e136ab0ddb7260a3fc92bc7da263c8ffefec62
SebDVS Web <69651784+sebdvsweb@users.noreply.github.com>
2025-04-11 08:37:08 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Quiz PHP</title>
+</head>
+<body>
+  <main class="container">
+    <h1>Quiz PHP</h1>
+  </main>
+</body>
+</html>
