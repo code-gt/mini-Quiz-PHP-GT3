6c28c554f39d7f8f2257d30132efa995b4458290
SebDVS Web <69651784+sebdvsweb@users.noreply.github.com>
2025-04-11 08:42:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+// Démarrer la session
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -5,8 +11,19 @@
   <title>Quiz PHP</title>
 </head>
 <body>
+  
   <main class="container">
     <h1>Quiz PHP</h1>
+
+    <?php
+
+    // Géstion du cookie contenant le prénom (utiliser un formulaire simple)
+
+    ?>
+
+    <!-- Création du formulaire HTML contenant le Quiz complet -->
+    
   </main>
+  
 </body>
 </html>
